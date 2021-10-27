44abb2385c893782ae766261cc321fad28239920
Joe Edward <joeedward@github.com>
2021-10-27 16:05:17 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Democrify.MixProject do
   def project do
     [
       app: :democrify,
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
